e7550a23bc92dabd6765c8f0e1ee3864162bb93c
Chris Bottin <chrisbottin@gmail.com>
2026-03-13 22:42:55 +0000

    Updated main branch

--- .github/scripts/publish.sh
+++ .github/scripts/publish.sh
@@ -10,8 +10,9 @@ echo "                                                              |___/ "
 echo "This script will publish a new version to NPM, create a version bump git commit, tag it and push it."
 
 branchName=`git rev-parse --abbrev-ref HEAD`
+mainBranchName="main"
 
-if [[ $branchName != "main" ]]; then
+if [[ $branchName != ${mainBranchName} ]]; then
   echo "Current branch is $branchName. Only the main branch can be published."
   exit 1
 fi
@@ -30,5 +31,5 @@ git tag $newVersion
 
 npm publish --provenance --ignore-scripts --verbose
 
-git push origin master
+git push origin ${mainBranchName}
 git push origin $newVersion
