23b9c98820ce93064e315eccfb96571ff997a20d
Chris Bottin <chrisbottin@gmail.com>
2026-03-13 22:08:19 +0000

    Initial commit

---
+++ .github/scripts/publish.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+echo ""
+echo "|************************|"
+echo "|     NPM Publishing     |"
+echo "|************************|"
+echo ""
+echo "This script will publish a new version to NPM, create a version bump git commit, tag it and push it."
+
+branchName=`git rev-parse --abbrev-ref HEAD`
+
+if [[ $branchName != "master" ]]; then
+  echo "Current branch is $branchName. Only the master branch can be published."
+  exit 1
+fi
+
+npm run compile
+npm run test
+
+npm version $VERSION_BUMP -m "Version Bump to %s ($VERSION_BUMP)"
+
+newVersion=`npm view . --silent version`
+
+git tag $newVersion
+
+npm publish --ignore-scripts
+
+git push origin master
+git push origin $newVersion
